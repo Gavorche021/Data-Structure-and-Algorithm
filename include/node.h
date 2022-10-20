0a9ad8ab62c5b7562807b89b07b5760b7983c807
Yichi Zhang <1813961736@qq.com>
2022-10-20 22:27:51 +0800

    add node

---
+++ include/node.h
@@ -0,0 +1,55 @@
+#pragma once
+#ifndef _NODE_
+#define _NODE_
+
+using namespace std;
+
+template <typename T>
+class Node{
+
+private:
+    int capacity;
+    T elem;
+    T lastNode;
+
+public:
+    T firstNode = nullptr;
+    T lastNode;
+
+    Node()=default;
+
+    //初始化一个节点（链表）
+    Node(T _elem){
+        if(firstNode == nullptr){
+            firstNode = _elem;
+            lastNode = _elem;
+            elem = _elem;
+            capacity = 1;
+        }
+        else{
+            throw;
+        }
+    };
+
+    ~Node(){
+
+    };
+
+    Node(const Node &copy){
+
+    };
+
+    void InsertElem(T value,int index){
+       
+    }
+    
+
+
+
+
+};
+
+
+#endif 
+
+
